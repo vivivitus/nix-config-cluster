bb737910831b2fe8e63ef3dada7456598c2a0e10
vivivitus <vivi_vitus@hotmail.com>
2026-08-10 16:47:45 +0200

    removed double storage definitions

--- host/common/hardware/cm3588plus.nix
+++ host/common/hardware/cm3588plus.nix
@@ -30,36 +30,10 @@
     interval = "weekly"; 
   };
 
-  # --- Dateisysteme & Subvolumes ---
   fileSystems."/" = {
     device = "none";
     fsType = "tmpfs";
     options = [ "defaults" "size=2G" "mode=755" ];
-  };
-
-  fileSystems."/persist" = {
-    device = "/dev/disk/by-label/root";
-    fsType = "btrfs";
-    options = [ "subvol=persist" "compress=zstd" "commit=120" "noatime" "nodiratime" ];
-    neededForBoot = true;
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-label/root";
-    fsType = "btrfs";
-    options = [ "subvol=home" "compress=zstd" "commit=120" "noatime" "nodiratime" ];
-  };
-
-  fileSystems."/nix" = {
-    device = "/dev/disk/by-label/root";
-    fsType = "btrfs";
-    options = [ "subvol=nix" "compress=zstd" "commit=120" "noatime" "nodiratime" "ro" ];
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/BOOT";
-    fsType = "vfat";
-    options = [ "fmask=0077" "dmask=0077" "noatime" ];
   };
 
   fileSystems."/etc/ssh" = {
