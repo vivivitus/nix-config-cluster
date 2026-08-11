bee6f5cf257040efd683a70e51211ed00f07700a
vivivitus <vivi_vitus@hotmail.com>
2026-08-12 00:35:51 +0200

    forgot import

--- host/n1/default.nix
+++ host/n1/default.nix
@@ -5,6 +5,7 @@
     ../common/global
     ../common/hardware/cm3588plus.nix
     ../common/hardware/disko.nix
+    ../common/hardware/impermanence.nix
     ../common/networking
     ../common/user/vivian
     ./k3s.nix
